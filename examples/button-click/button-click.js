5cda43fd9ae8850b591c5f8e3dada7dc37070a41
chandan Kumar <chandan.kumar@emirates.com>
2018-11-10 21:55:43 +0400

    Added feature to allow test file as spec.js & Extended it to create the storybook component

---
+++ examples/button-click/button-click.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class BUTTONCLICK extends Component {
+  render() {
+    return <div>BUTTONCLICK</div>;
+  }
+}
+
+BUTTONCLICK.propTypes = {};
+
+export default BUTTONCLICK;
