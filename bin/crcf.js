beb8c84ea738d438ac3e08a922f32daa40e74e03
David Barker <david.aw.barker@gmail.com>
2019-05-22 00:20:24 +0100

    - Added template publishing - Split application into two sub commands with the default set to create for backward compatibility. - Added argument to logComponentTree to allow control over appending the component name to the folderPath

--- bin/crcf.js
+++ bin/crcf.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
-require('../lib/index');
+const program = require('commander');
+
+program.version('0.2.0')
+  .command('publish-templates', 'Publish template files to a project')
+  .command('create', 'Create a react component folder', { isDefault: true })
+  .parse(process.argv);
