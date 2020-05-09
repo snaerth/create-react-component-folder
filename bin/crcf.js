6b6fce072f2f4b503f3e0e96c78571edd196ddc0
David Barker <david.aw.barker@gmail.com>
2020-05-09 10:41:04 +0100

    - Updated CRCF runtime error catching - Fixed proptype errors in templates

--- bin/crcf.js
+++ bin/crcf.js
@@ -2,7 +2,11 @@
 
 const program = require('commander');
 
-program.version('0.2.0')
-  .command('publish-templates', 'Publish template files to a project')
-  .command('create', 'Create a react component folder', { isDefault: true })
-  .parse(process.argv);
+try {
+  program.version('0.2.0')
+    .command('publish-templates', 'Publish template files to a project')
+    .command('create', 'Create a react component folder', { isDefault: true })
+    .parse(process.argv);
+} catch(error) {
+  logger.error('Invalid command', error);
+}
